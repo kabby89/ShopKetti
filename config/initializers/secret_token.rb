c390b6f45db7e4409eebbb30924dc7a1ce34950c
Kellen McCarvel <Kellen@Kellens-MacBook-Pro.local>
2015-01-09 06:33:10 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Shopketti::Application.config.secret_key_base = '721676d4cfab92a0e8274a0ec453d4b122a13560168710029c3fffeb873b24b566b505d2f42a3bb7fac219dd33ab4a4bfd48c506fbb975fe33479675a1fb4a4e'
