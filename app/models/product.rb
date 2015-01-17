359a5d6e25ae0b67f16210bc7fafad3f8ce47fcc
Kellen McCarvel <Kellen@Kellens-MacBook-Pro.local>
2015-01-17 11:28:44 -0700

    heroku update 2

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,6 @@ class Product < ActiveRecord::Base
 	belongs_to :user
 	accepts_nested_attributes_for :tasks, :reject_if => :all_blank, :allow_destroy => true
 	has_many :sku_databases
+	has_many :colors
+	has_many :sizes
 end
