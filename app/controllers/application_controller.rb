3c96f6314684a8ff7e66abf7212d5d481f588362
Kellen McCarvel <Kellen@Kellens-MacBook-Pro.local>
2015-01-20 22:41:11 -0500

    marketplace update 2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,13 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  def devise_paramter_sanitizer
+  	if resource_class == User
+  		User::ParameterSanitizer.new(User, :user, :seller_company_name)
+  	else
+  		super
+  	end
+  end
   	protected
 
   	def configure_permitted_parameters
--- app/controllers/static_home_page_controller.rb
+++ app/controllers/static_home_page_controller.rb
@@ -1,11 +1,11 @@
 class StaticHomePageController < ApplicationController
 	def index
-		if current_user.user_type == 'retailer'
-			redirect_to root_path
-		elsif current_user.user_type == 'creator'
-			redirect_to creator_dashboards_path
-		else
-		 	redirect_to root_path
+		if user_signed_in? != true
+			root_url
+		elsif current_user.user_type == 'retailer'
+			redirect_to market_stores_path
+		else 
+			redirect_to creator_dashboards_path	
 		end
 	end
 end
