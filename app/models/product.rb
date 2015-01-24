74a19e2c304732b5033f2614a1ba6189cfe70e74
Kellen McCarvel <Kellen@Kellens-MacBook-Pro.local>
2015-01-24 12:09:36 -0500

    multiple images uploader

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,5 @@
 class Product < ActiveRecord::Base
-	mount_uploader :image, ImageUploader
+	mount_uploader :images, ImageUploader
 
 	belongs_to :user
 	has_many :colors
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150124163533) do
+ActiveRecord::Schema.define(version: 20150124165155) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,7 +31,6 @@ ActiveRecord::Schema.define(version: 20150124163533) do
     t.string   "style_number"
     t.decimal  "shipping_cost", precision: 19, scale: 2
     t.integer  "user_id"
-    t.string   "image"
     t.json     "images"
   end
 
